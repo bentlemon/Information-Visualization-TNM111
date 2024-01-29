47982c9f1f662f8d023e2140572f9d830b0de381
MB <93658035+bentlemon@users.noreply.github.com>
2024-01-29 14:54:38 +0100

    Task 3 beginning
    Created a virtual enviroment for pyhton and made a small pop-up window with Tkinter

---
+++ Infovis_task3/main.py
@@ -0,0 +1,12 @@
+# När man är i "Infovis_task3" foldern skriv: .venv\Scripts\activate i cmd för att få den att aktivera 
+# virtuella miljön (virtual enviroment). Är korrekt om det står: (.venv) C:\Users\mahon\Desktop\Infovis_task3> i terminalen
+# Jag följde 2 olika tutorials: 
+# https://www.youtube.com/watch?v=GrAYoJUQolo (för att starta upp miljön)
+# https://code.visualstudio.com/docs/python/environments (För att få igång python)
+
+# Importera GUI toolkitet  
+from tkinter import *
+
+# skapar ett litet testfönster!
+main = Tk()
+main.mainloop()
