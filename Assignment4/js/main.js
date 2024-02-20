140bffb3ce6bd7be8449e17f626953fa3b705f8c
Rebecka Sahlin <rebsa125@student.liu.se>
2024-02-20 16:23:05 +0100

    Dropdown menu
    addEventListener vill egentligen inte vara en funktion, men den fungerar

--- Assignment4/js/main.js
+++ Assignment4/js/main.js
@@ -1,19 +1,49 @@
-(async function() {
+let jsonFiles = [
+  { name: "Star Wars Episode 1", url: "starwars-episode-1-interactions-allCharacters.json" },
+  { name: "Star Wars Episode 2", url: "starwars-episode-2-interactions-allCharacters.json" },
+  { name: "Star Wars Episode 3", url: "starwars-episode-3-interactions-allCharacters.json" },
+  { name: "Star Wars Episode 4", url: "starwars-episode-4-interactions-allCharacters.json" },
+  { name: "Star Wars Episode 5", url: "starwars-episode-5-interactions-allCharacters.json" },
+  { name: "Star Wars Episode 6", url: "starwars-episode-6-interactions-allCharacters.json" },
+  { name: "Star Wars Episode 7", url: "starwars-episode-7-interactions-allCharacters.json" },
+  { name: "Star Wars Episode all", url: "starwars-full-interactions-allCharacters.json" }
+];
+// Dropdown menu
+var select = document.createElement("select");
+select.id = "jsonSelect";
+// Lägger till alla alternativ att välja emellan
+jsonFiles.forEach(function(file) {
+  var option = document.createElement("option");
+  option.value = file.url;
+  option.text = file.name;
+  select.appendChild(option);
+});
+
+document.body.appendChild(select);
+select.addEventListener("change", async function() {
+  var selectedValue = this.value;
+  var jsonURL = "./data/" + selectedValue;
+  try {
+    var data = await d3.json(jsonURL);
+    console.log("Data loaded successfully:", data);
+    // Här kan du göra vad du vill med den laddade datan
+
+
+
+/*(async function() {
   let data;
   try {
     data = await d3.json("./data/starwars-episode-1-interactions-allCharacters.json");
   } catch (error) {
     console.error("Error loading data:", error);
     return; // Exit function if data loading fails
-  }
+  }*/
 
 
  /* const contentDiv = document.getElementById("visualization");
 
   const width = contentDiv.clientWidth;
   const height = contentDiv.clientHeight;*/
-
-
 
   var width = 450, height = 340
 
@@ -62,7 +92,10 @@
     updateNodes()
   }
   
-
-  //document.getElementById("visualization").appendChild(networkDiagram);
+} catch (error) {
+  console.error("Error loading data:", error);
+};
 
 })();
+
+
