ae1826afece0c16b42844744006bd419c59562da
Rebecka Sahlin <rebsa125@student.liu.se>
2024-02-20 13:44:47 +0100

    mycket fel, men kan gå tillbaka

--- Assignment4/js/main.js
+++ Assignment4/js/main.js
@@ -1,7 +1,7 @@
 (async function() {
   let data;
   try {
-    data = await d3.json("./data/starwars-full-interactions-allCharacters.json");
+    data = await d3.json("./data/starwars-episode-1-interactions-allCharacters.json");
   } catch (error) {
     console.error("Error loading data:", error);
     return; // Exit function if data loading fails
@@ -21,42 +21,45 @@
     var nodes = data.nodes;
     var links = data.links;
 
-    console.log(data)
     var simulation = d3.forceSimulation(nodes)
                        .force("charge", d3.forceManyBody().strength(-50))
                        .force("link", d3.forceLink().links(links))
                        .force("center", d3.forceCenter(width / 2, height / 2))
                        .on("tick", ticked);
 
+
     function updateLinks() {
-                        svg.append("g")
-                         d3.selectAll(".links") 
-                          .data(links)
-                          .append("line")
-                          .attr("stroke", "black")
-                          .attr("x1", function(d) { return d.source.x; })
-                          .attr("y1", function(d) { return d.source.y; })
-                          .attr("x2", function(d) { return d.target.x; })
-                          .attr("y2", function(d) { return d.target.y; });
+                        svg.selectAll(".link")
+                           .data(links)
+                           .join("line")
+                           .attr("class", "link")
+                           .attr("stroke", "black")
+                           .attr("x1", function(d) { return d.source.x; })
+                           .attr("y1", function(d) { return d.source.y; })
+                           .attr("x2", function(d) { return d.target.x; })
+                           .attr("y2", function(d) { return d.target.y; });
                       }
-                      
+                  
     function updateNodes() {
-                        d3.selectAll("circle") 
-                          .data(nodes)
-                          .join("circle")
-                          .attr("r", 5)
-                          .attr("fill", "red")
-                          .attr("x", function(d) { return d.x; })
-                          .attr("y", function(d) { return d.y; });
-                      }
+                        svg.selectAll("circle")
+                           .data(nodes)
+                           .join("circle")
+                           .attr("class", "node")
+                           .attr("r", function(d) { return d.value; }) // Använd 'value' för att sätta storleken på noderna
+                           .attr("fill", function(d) { return d.colour; }) // Använd 'colour' för att sätta färgen på noderna
+                           .attr("cx", function(d) { return d.x; })
+                           .attr("cy", function(d) { return d.y; });
+
+    }
 
     function ticked() {
    
       updateLinks();
       updateNodes();
+     
     }
-
     return svg.node();
+ 
   }
 
   var networkDiagram = createNetworkDiagram(data);
