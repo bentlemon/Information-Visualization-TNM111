fe55c83109d16b80db552504e23b862e691b71c9
Rebecka Sahlin <rebsa125@student.liu.se>
2024-02-20 14:03:37 +0100

    FUNGERAR

--- Assignment4/js/main.js
+++ Assignment4/js/main.js
@@ -7,60 +7,68 @@
     return; // Exit function if data loading fails
   }
 
-  function createNetworkDiagram(data) {
+ /* function createNetworkDiagram(data) {
   const contentDiv = document.getElementById("visualization");
 
   const width = contentDiv.clientWidth;
-  const height = contentDiv.clientHeight;
-
-    var svg = d3.select("#visualization")
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height);
-
-    var nodes = data.nodes;
-    var links = data.links;
-
-    var simulation = d3.forceSimulation(nodes)
-                       .force("charge", d3.forceManyBody().strength(-50))
-                       .force("link", d3.forceLink().links(links))
-                       .force("center", d3.forceCenter(width / 2, height / 2))
-                       .on("tick", ticked);
+  const height = contentDiv.clientHeight;*/
 
 
-    function updateLinks() {
-                        svg.selectAll(".link")
-                           .data(links)
-                           .join("line")
-                           .attr("class", "link")
-                           .attr("stroke", "black")
-                           .attr("x1", function(d) { return d.source.x; })
-                           .attr("y1", function(d) { return d.source.y; })
-                           .attr("x2", function(d) { return d.target.x; })
-                           .attr("y2", function(d) { return d.target.y; });
-                      }
-                  
-    function updateNodes() {
-                        svg.selectAll("circle")
-                           .data(nodes)
-                           .join("circle")
-                           .attr("class", "node")
-                           .attr("r", function(d) { return d.value; }) // Använd 'value' för att sätta storleken på noderna
-                           .attr("fill", function(d) { return d.colour; }) // Använd 'colour' för att sätta färgen på noderna
-                           .attr("cx", function(d) { return d.x; })
-                           .attr("cy", function(d) { return d.y; });
 
-    }
+  var width = 400, height = 300
 
-    function ticked() {
-   
-      updateLinks();
-      updateNodes();
-     
-    }
-    return svg.node();
- 
+  let nodes = data.nodes;
+  let links = data.links;
+
+  var simulation = d3.forceSimulation(nodes)
+    .force('charge', d3.forceManyBody().strength(-100))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('link', d3.forceLink().links(links))
+    .on('tick', ticked);
+  
+  function updateLinks() {
+    var u = d3.select('.links')
+      .selectAll('line')
+      .data(links)
+      .join('line')
+      .attr('x1', function(d) {
+        return d.source.x
+      })
+      .attr('y1', function(d) {
+        return d.source.y
+      })
+      .attr('x2', function(d) {
+        return d.target.x
+      })
+      .attr('y2', function(d) {
+        return d.target.y
+      });
   }
+  
+  function updateNodes() {
+    u = d3.select('.nodes')
+      .selectAll('text')
+      .data(nodes)
+      .join('text')
+      .text(function(d) {
+        return d.name
+      })
+      .attr('x', function(d) {
+        return d.x
+      })
+      .attr('y', function(d) {
+        return d.y
+      })
+      .attr('dy', function(d) {
+        return 5
+      });
+  }
+  
+  function ticked() {
+    updateLinks()
+    updateNodes()
+  }
+  
 
   var networkDiagram = createNetworkDiagram(data);
   document.getElementById("visualization").appendChild(networkDiagram);
