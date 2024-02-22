90b9450d49cf773bdb95c2a7cbaa255229059408
MB <bengtssonmolly185@gmail.com>
2024-02-22 17:50:07 +0100

    2 div 2 drop - Big spagettikod

--- Assignment4/js/main.js
+++ Assignment4/js/main.js
@@ -9,40 +9,40 @@ let jsonFiles = [
   { name: "Star Wars Episode all", url: "starwars-full-interactions-allCharacters.json" }
 ];
 
-// Dropdown menu
-var select = document.createElement("select");
-select.id = "jsonSelect";
+// Dropdown menu for content1
+var select1 = document.createElement("select");
+select1.id = "jsonSelect1";
 
 // Add all options to choose from
 jsonFiles.forEach(function(file) {
   var option = document.createElement("option");
   option.value = file.url;
   option.text = file.name;
-  select.appendChild(option);
+  select1.appendChild(option);
 });
 
-document.body.appendChild(select);
+document.getElementById("content1").appendChild(select1);
 
-select.addEventListener("change", async function() {
+select1.addEventListener("change", async function() {
   var selectedValue = this.value;
   var jsonURL = "./data/" + selectedValue;
-  
+
   try {
     var data = await d3.json(jsonURL);
-    console.log("Data loaded successfully:", data);
+    console.log("Data loaded successfully for content1:", data);
 
     var width = 450, height = 340;
     let nodes = data.nodes;
     let links = data.links;
-    
+
     var simulation = d3.forceSimulation(nodes)
       .force('charge', d3.forceManyBody().strength(-100))
       .force('center', d3.forceCenter(width / 2, height / 2))
       .force('link', d3.forceLink().links(links))
       .on('tick', ticked);
-  
+
     function updateLinks() {
-      var u = d3.select('.links')
+      var u = d3.select('#content1 .links')
         .selectAll('line')
         .data(links)
         .join('line')
@@ -51,10 +51,10 @@ select.addEventListener("change", async function() {
         .attr('x2', function(d) { return d.target.x })
         .attr('y2', function(d) { return d.target.y });
     }
+
     var selectedNode = null;
-    var selectedNodeData;
     function updateNodes() {
-      d3.select('.nodes')
+      d3.select('#content1 .nodes')
         .selectAll('circle')
         .data(nodes)
         .join('circle')
@@ -66,43 +66,101 @@ select.addEventListener("change", async function() {
         .on("mouseover", function(event, d) {
           showTooltip(d);
           if (selectedNode !== null && selectedNode !== this) {
-            // Återställ den senaste valda nodens utseende
             d3.select(selectedNode).classed("highlighted", false);
           }
           var isSelected = d3.select(this).classed("highlighted");
           d3.select(this).classed("highlighted", !isSelected);
-          selectedNode = this; // Uppdatera den senaste valda noden
+          selectedNode = this;
         })
         .on("mouseout", function(event, d) {
           hideTooltip();
         });
     }
 
-    function tooltipContent(d) {
-      var content = "";
-      content += "Name: " + d.name + "<br/>";
-      content += "Value: " + d.value + "<br/>";
-      // Om du har fler attribut, kan du lägga till dem här
-      return content;
+    function ticked() {
+      updateLinks();
+      updateNodes();
     }
-  
-    function showTooltip(d) {
-      var tooltip = d3.select("#tooltip");
-      tooltip.html(tooltipContent(d));
-      tooltip.style("opacity", 0.9);
+
+  } catch (error) {
+    console.error("Error loading data for content1:", error);
+  }
+});
+
+// Dropdown menu for content2
+var select2 = document.createElement("select");
+select2.id = "jsonSelect2";
+
+// Add all options to choose from
+jsonFiles.forEach(function(file) {
+  var option = document.createElement("option");
+  option.value = file.url;
+  option.text = file.name;
+  select2.appendChild(option);
+});
+
+document.getElementById("content2").appendChild(select2);
+
+select2.addEventListener("change", async function() {
+  var selectedValue = this.value;
+  var jsonURL = "./data/" + selectedValue;
+
+  try {
+    var data = await d3.json(jsonURL);
+    console.log("Data loaded successfully for content2:", data);
+
+    var width = 450, height = 340;
+    let nodes = data.nodes;
+    let links = data.links;
+
+    var simulation = d3.forceSimulation(nodes)
+      .force('charge', d3.forceManyBody().strength(-100))
+      .force('center', d3.forceCenter(width / 2, height / 2))
+      .force('link', d3.forceLink().links(links))
+      .on('tick', ticked);
+
+    function updateLinks() {
+      var u = d3.select('#content2 .links')
+        .selectAll('line')
+        .data(links)
+        .join('line')
+        .attr('x1', function(d) { return d.source.x })
+        .attr('y1', function(d) { return d.source.y })
+        .attr('x2', function(d) { return d.target.x })
+        .attr('y2', function(d) { return d.target.y });
     }
-    
-    function hideTooltip() {
-      var tooltip = d3.select("#tooltip");
-      tooltip.style("opacity", 0.9); // Kan sätta 0 om man vill att rutan ska "försvinna"
+
+    var selectedNode = null;
+    function updateNodes() {
+      d3.select('#content2 .nodes')
+        .selectAll('circle')
+        .data(nodes)
+        .join('circle')
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; })
+        .attr("r", function(d) { return d.value; })
+        .attr("fill", function(d) { return d.colour; })
+        .attr("opacity", 0.7)
+        .on("mouseover", function(event, d) {
+          showTooltip(d);
+          if (selectedNode !== null && selectedNode !== this) {
+            d3.select(selectedNode).classed("highlighted", false);
+          }
+          var isSelected = d3.select(this).classed("highlighted");
+          d3.select(this).classed("highlighted", !isSelected);
+          selectedNode = this;
+        })
+        .on("mouseout", function(event, d) {
+          hideTooltip();
+        });
     }
 
     function ticked() {
       updateLinks();
       updateNodes();
     }
-  
+
   } catch (error) {
-    console.error("Error loading data:", error);
+    console.error("Error loading data for content2:", error);
   }
 });
